2292fec5cad090d7dabc0edcba8da2d3ffd6af0b
TheOldRoe <karaca.tolga1@gmail.com>
2019-12-09 19:51:14 +0300

    ilk degisiklik

--- enBuyukSayi.py
+++ enBuyukSayi.py
@@ -4,6 +4,8 @@ sayi1 = int(input("1. Sayı :"))
 sayi2 = int(input("2. Sayı :"))
 sayi3 = int(input("3. Sayı :"))
 
+#deneme eklentisi
+
 if (sayi1>=sayi2) and (sayi1>=sayi3):
     enBuyuk = sayi1
 elif (sayi2>=sayi1) and (sayi2>=sayi3):
